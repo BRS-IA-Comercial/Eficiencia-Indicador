ffba76eaecba518cb875f1e3b3cbff6c5f7513ed
BRS IA <brs.ia@brsupply.com.br>
2026-03-05 12:13:06 +0000

    Try fixing this error: `Build Error: Export firebaseConfig doesn't exist

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAz-placeholder-key",
+  apiKey: "AIzaSyC5LTvb8f5TQueHcT2xVKFDlwlz2fKJhSA",
   authDomain: "studio-1889772732-ebc5b.firebaseapp.com",
   projectId: "studio-1889772732-ebc5b",
   storageBucket: "studio-1889772732-ebc5b.firebasestorage.app",
   messagingSenderId: "903686538879",
-  appId: "1:903686538879:web:placeholder-id",
+  appId: "1:903686538879:web:d529381cc4a436b979a14a"
 };
